6d774a547947e9e51a2c019d8252e263b0289c02
Narngisa <nattanan43658@gmail.com>
2026-06-04 21:05:14 +0700

    fix calculate Ideal gas

--- pyIdeal_gas.py
+++ pyIdeal_gas.py
@@ -1,19 +1,39 @@
+from dataclasses import dataclass, KW_ONLY
+
+@dataclass
 class PVnRT:
-    def __init__(self, pressure: float, volume: float, mol: float, temperature: float):
-        self.pressure = pressure
-        self.volume = volume
-        self.mol = mol
-        self.gas_constant = 0.0821
-        self.temperature = temperature
+    _: KW_ONLY
+    pressure: float | None = None
+    volume: float | None = None
+    mol: float | None = None
+    gas_constant: float = 0.0821
+    temperature: float | None = None
 
-    def calculate_pressure(self):
-        return (self.mol * self.gas_constant * self.temperature) / self.volume
+    @property
+    def _PVnRT(self):
+        if self.pressure is None:
+            if self.volume is None or self.mol is None or self.temperature is None:
+                raise ValueError("volume, mol, and temperature must be set to solve for pressure.")
+            self.pressure = (self.mol * self.gas_constant * self.temperature) / self.volume
+            return self.pressure
 
-    def calculate_volume(self):
-        return (self.mol * self.gas_constant * self.temperature) / self.pressure
+        elif self.volume is None:
+            if self.pressure is None or self.mol is None or self.temperature is None:
+                raise ValueError("pressure, mol and temperature must be set to solve for volume.")
+            self.volume = (self.mol * self.gas_constant * self.temperature) / self.pressure
+            return self.volume
 
-    def calculate_mol(self):
-        return (self.pressure * self.volume) / (self.gas_constant * self.temperature)
+        elif self.mol is None:
+            if self.pressure is None or self.volume is None or self.temperature is None:
+                raise ValueError("pressure, volume and temperature must be set to solve for mol.")
+            self.mol = (self.pressure * self.volume) / (self.gas_constant * self.temperature)
+            return self.mol
 
-    def calculate_temperature(self):
-        return (self.pressure * self.volume) / (self.mol * self.gas_constant)
+        elif self.temperature is None:
+            if self.pressure is None or self.volume is None or self.mol is None:
+                raise ValueError("pressure, volume and mol must be set to solve for temperature.")
+            self.temperature = (self.pressure * self.volume) / (self.mol * self.gas_constant)
+            return self.temperature
+
+        else:
+            raise ValueError("All values are already set — nothing to solve! !!")
