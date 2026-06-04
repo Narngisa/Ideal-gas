f472c269390a58487c1db5543d7980e906dfceb3
Natthanan <nattanan43658@gmail.com>
2026-06-04 16:53:14 +0700

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import pyIdeal_gas
-
-def main():
-    gas = pyIdeal_gas.PVnRT(pressure=0, volume=5, mol=7, temperature=300)
-    print(gas.calculate_pressure())
-
-if __name__ == "__main__":
-    main()
