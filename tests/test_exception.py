5966bab723845bd62a2f6b5430d5418a33b0b15a
Narngisa <nattanan43658@gmail.com>
2026-06-18 22:24:23 +0700

    Built test exception function

--- tests/test_exception.py
+++ tests/test_exception.py
@@ -1,15 +1,17 @@
-from ideal_gas import Density, PVdRT, PVnRT, PVgMRT, Pressure, Volume, Mole, Temperature, Gram, MolarMass
+from ideal_gas import PMRT, Density, Molarity, PVdRT, PVnRT, PVgMRT, Pressure, Volume, Mole, Temperature, Gram, MolarMass
 import pytest
 
 def test_raise_pressure_exits():
     gas_PV_nRT = PVnRT(pressure=Pressure(pressure=1, unit="atm"))
     gas_PV_gMRT = PVgMRT(pressure=Pressure(pressure=1, unit="atm"))
     gas_PV_dRT = PVdRT(pressure=Pressure(pressure=1, unit="atm"))
+    gas_P_MRT = PMRT(pressure=Pressure(pressure=1, unit="atm"))
 
     with pytest.raises(ValueError, match="pressure already exists"):
         gas_PV_nRT.calculate_pressure
         gas_PV_gMRT.calculate_pressure
         gas_PV_dRT.calculate_pressure
+        gas_P_MRT.calculate_pressure
 
 def test_raise_volume_exits():
     gas_PV_nRT = PVnRT(volume=Volume(volume=1000, unit="cm3"))
@@ -31,11 +33,13 @@ def test_raise_temperature_exits():
     gas_PV_nRT = PVnRT(temperature=Temperature(temperature=300, unit="K"))
     gas_PV_gMRT = PVgMRT(temperature=Temperature(temperature=300, unit="K"))
     gas_PV_dRT = PVdRT(temperature=Temperature(temperature=300, unit="K"))
+    gas_P_MRT = PMRT(temperature=Temperature(temperature=300, unit="K"))
 
     with pytest.raises(ValueError, match="temperature already exists"):
         gas_PV_nRT.calculate_temperature
         gas_PV_gMRT.calculate_temperature
         gas_PV_dRT.calculate_temperature
+        gas_P_MRT.calculate_temperature
 
 def test_raise_gram_exits():
     gas_PV_gMRT = PVgMRT(gram=Gram(gram=10, unit="g"))
@@ -54,3 +58,9 @@ def test_raise_density_exits():
 
     with pytest.raises(ValueError, match="density already exists"):
         gas_PV_dRT.calculate_density
+
+def test_raise_molarity_exits():
+    gas_P_MRT = PMRT(molarity=Molarity(molarity=6, unit="mol/L"))
+
+    with pytest.raises(ValueError, match="molarity already exists"):
+        gas_P_MRT.calculate_molarity
